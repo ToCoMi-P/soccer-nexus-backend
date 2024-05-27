84e719660a07ea2c0cded0041b5bb9807cf61625
A1QH2OG <ttran@cgs-online.de>
2024-05-27 17:37:40 +0200

    - remove Player Functionality added

--- src/main/java/com/kickerz73/soccernexus_backend/controller/PlayerAppliesController.java
+++ src/main/java/com/kickerz73/soccernexus_backend/controller/PlayerAppliesController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -15,14 +16,14 @@ import java.util.*;
 import java.time.Instant;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin//(origins = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
 public class PlayerAppliesController {
 
     private static final LocalDate currentMonday = LocalDate.of(2024, 5, 20);
 
-    private static final String PATTERN_FORMAT = "dd.MM.yyyy HH:mm:ss";
+    private static final String PATTERN_FORMAT = "dd.MM.yyyy HH:mm:ss.SSS";
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(PATTERN_FORMAT).withZone(ZoneId.systemDefault());
 
     @Autowired final PlayerRepository playerRepository;
@@ -60,7 +61,6 @@ public class PlayerAppliesController {
     @PostMapping("/playersapplies/{id}")
     void addPlayer(@RequestParam Long id){
         PlayerEntity p = playerRepository.getReferenceById(id);
-
         PlayerApplies pa = new PlayerApplies(p, formatter.format(Instant.now()), currentMonday);
         playerAppliesRepository.save(pa);
     }
@@ -74,10 +74,12 @@ public class PlayerAppliesController {
 
     @PostMapping("/removeplayer/{id}")
     void removePlayer(@RequestParam Long id){
-        /*PlayerEntity p = playerRepository.getReferenceById(id);
+        PlayerEntity toRemovedPlayer = playerRepository.getReferenceById(id);
 
-        PlayerApplies pa = new PlayerApplies(p, formatter.format(Instant.now()), currentMonday);
-        playerAppliesRepository.save(pa);*/
+        Optional<PlayerApplies> optPA = playerAppliesRepository.findAll().stream().filter(x-> x.getPlayer().equals(toRemovedPlayer)).findFirst();
+        if(optPA.isPresent()){
+            playerAppliesRepository.delete(optPA.get());
+        }
     }
 
     @PostMapping("/removeplayer")
